4de868f68788d10b4d572ce1111998b68481c32a
Elanur Yavuz <elasf4@gmail.com>
2025-12-10 21:21:09 +0300

    Proje dosyası ekle.

---
+++ odev1/Models/UserDetails.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+
+namespace odev1.Models
+{
+    public class UserDetails :IdentityUser
+    {
+        [Required]
+        [MaxLength(30, ErrorMessage ="İsminiz 30 karakterden uzun olamaz.")]
+        [MinLength(3, ErrorMessage ="İsminiz 3 karakterden kısa olamaz.")]
+        public string userAd { get; set; }
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "Soyisminiz 30 karakterden uzun olamaz.")]
+        [MinLength(2, ErrorMessage = "Soyisminiz 2 karakterden kısa olamaz.")]
+        public string userSoyad { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string userMail { get; set; }
+
+        [Required]
+        [Phone] //???? salladım
+        public int userTel { get; set; }
+
+
+        //password????
+
+
+    }
+}
